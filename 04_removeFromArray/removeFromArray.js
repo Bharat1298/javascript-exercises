93ce5e93c99253583bc3f616924cec3f35e7b367
Bharat <BharatKhadka1298@gmail.com>
2024-09-05 01:29:01 -0400

    Finished JavaScript excersises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, arg, ...otherArgs) {
+    otherArgs.unshift(arg);
+    for(let i = 0; i < otherArgs.length; i++){
+        while(arr.includes(otherArgs[i])){
+            for(let j = 0; j < arr.length; j++){
+                if(arr[j] === otherArgs[i]){
+                    arr.splice(j, 1);
+                    j -= 1
+                }
+            }
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
